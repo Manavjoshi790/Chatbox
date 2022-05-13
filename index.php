f104b2b7a769dbde3f3fbf4c6dbb97a98dab202f
manav <manavjoshi790@gmail.com>
2022-05-13 15:39:20 +0530

    content change

--- index.php
+++ index.php
@@ -24,28 +24,28 @@
                 <div class="name-details">
                     <div class="field input">
                         <label>First Name</label>
-                        <input type="text" placeholder="First Name">
+                        <input type="text" name="fname" placeholder="First Name" required>
                     </div>
 
                     <div class="field input">
                         <label>Last Name</label>
-                        <input type="text" placeholder="Last Name">
+                        <input type="text" name="lname" placeholder="Last Name" required>
                     </div>
                 </div>
                 <div class="field input">
                     <label>Email Address</label>
-                    <input type="text" placeholder="Enter Your Email">
+                    <input type="text" name="email" placeholder="Enter Your Email" required>
                 </div>
 
                 <div class="field input">
                     <label>Password</label>
-                    <input type="password" placeholder="Enter New Password">
+                    <input type="password" name="password" placeholder="Enter New Password" required>
                     <i class="fas fa-eye"></i>
                 </div>
 
                 <div class="field image">
                     <label>Select Image</label>
-                    <input type="file">
+                    <input type="file" name="image" required>
                 </div>
 
                 <div class="field button">
@@ -56,6 +56,8 @@
 <div class="link">Already Signed Up! <a href="#">Login now</a></div>
         </section>
     </div>
+    <script src="javascript/pass-show-hide.js"></script>
+    <script src="javascript/signup.js"></script>
+
 </body>
-<script src="javascript/pass-show-hide.js"></script>
 </html>
