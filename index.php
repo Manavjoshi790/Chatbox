d95beecbf1711449572208fdfff5febb1503ec23
manav <manavjoshi790@gmail.com>
2022-05-14 14:40:58 +0530

    content change

--- index.php
+++ index.php
@@ -17,10 +17,8 @@
             <header>
                 Realtime Chat App
             </header>
-            <form action="#">
-                <div class="error-txt">
-                    This is an error message!
-                </div>
+            <form action="#" enctype="multipart/form-data">
+                <div class="error-txt"></div>
                 <div class="name-details">
                     <div class="field input">
                         <label>First Name</label>
